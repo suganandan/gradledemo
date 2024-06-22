6919a17172d4cefb62a2d2c9bd5ae983809dd95e
suganandan <suganandan@hotmail.com>
2024-06-22 12:28:24 +0530

    first commit

---
+++ src/main/java/com/example/gradledemo/GradledemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.gradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradledemoApplication.class, args);
+	}
+
+}
+git
